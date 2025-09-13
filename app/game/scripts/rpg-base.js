f11552bf3288cd34f18664bfa301bbf2b8ba7002
iain @ Garnet <iainvisa@gmail.com>
2025-09-13 18:56:18 +0200

    rpg base

--- app/game/scripts/rpg-base.js
+++ app/game/scripts/rpg-base.js
@@ -4,7 +4,7 @@
 
 
 
-async function renderRPGMap(mapName){
+async function renderRPGMap(mapName, mapContainer=undefined){
     let mapData = await fetch("/rpg/map",
         {
             method: "POST",
@@ -15,6 +15,12 @@ async function renderRPGMap(mapName){
         }
     );
     console.log(mapData);
+    if (mapContainer === undefined){
+        mapContainer = document.getElementsByClassName("rpg-map")[0];
+    }
+    [...mapContainer.children].forEach(c => {if (!c.classList.contains("template")){c.remove();}});
+
+
 }
 
 
