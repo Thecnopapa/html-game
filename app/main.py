d2ad136bb57641d04ddb738bec5b3d75eb403535
iain @ Garnet <iainvisa@gmail.com>
2025-09-13 17:35:47 +0200

    main menu

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='game/templates')
 
-app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("main-menu.html")
+
+
+
+
 
 
 def main():
