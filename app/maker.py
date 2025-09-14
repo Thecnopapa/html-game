cdd0701cd36185affd2176b2b1c0e981ce0ef2a1
iainv <iainvisa@gmail.com>
2025-09-14 13:09:53 +0200

    game maker setup

---
+++ app/maker.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, url_for, send_file
+import requests
+import pandas as pd
+import os
+import json
+import random
+from PIL import Image
+import io
+app = Flask(__name__, template_folder='maker/templates', static_folder='maker')
+
+
+SIZE = 32
+
+
+@app.route("/style/<file>")
+def return_style(file):
+    return app.send_static_file("style/" + file)
+
+@app.route("/scripts/<file>")
+def return_scripts(file):
+    return app.send_static_file("scripts/" + file)
+
+@app.route("/")
+def index():
+    return render_template("maker-menu.html")
+
+@app.route("/editor/<style>/<asset>")
+def open_editor(style, asset):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    app.run(port=4242, host="0.0.0.0", debug=True) # Not used if run from bash
+
+if __name__ == "__main__":
+    main()
